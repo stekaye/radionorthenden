e9fe7567336f95b7c583eeaa3a70600f03846073
Stephen Kaye <stephenkayebarcelona@gmail.com>
2020-07-13 17:26:05 +0200

    Test PHP

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -15,26 +15,31 @@
     header("Location: index.php?mailsend");
   }
 
-   //Store name of users
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-
-  $email_from = 'tostephenkaye@gmail.com';
-
-  $email_subject = "New Form Submission";
-
-  $email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "User Message: $message./n";
-
-  $to = 'stephenkayebarcelona@gmail.com';
-
-  $headers = "From: $email_from \r\n";
-
-  $headers = "Reply-To: $visitor_email \r\n";
-
-  mail($to, $email_subject, $email_body, $headers);
-
-  header("Location: index.html");
-
 ?>
+
+
+
+
+
+
+<!-- //  //Store name of users
+  // $visitor_email = $_POST['email'];
+  // $message = $_POST['message'];
+
+  // $email_from = 'tostephenkaye@gmail.com';
+
+  // $email_subject = "New Form Submission";
+
+  // $email_body = "User Name: $name.\n".
+  //               "User Email: $visitor_email.\n".
+  //               "User Message: $message./n";
+
+  // $to = 'stephenkayebarcelona@gmail.com';
+
+  // $headers = "From: $email_from \r\n";
+
+  // $headers = "Reply-To: $visitor_email \r\n";
+
+  // mail($to, $email_subject, $email_body, $headers);
+
+  // header("Location: index.html"); -->
